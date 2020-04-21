f959ffdcdc007b7770f30d79bc2efe490ddffe5c
AllanMar <allanmar12@gmail.com>
2020-04-21 13:42:58 -0300

    Change HLT offset to 2.2deg

--- Config.h
+++ Config.h
@@ -28,7 +28,7 @@
 // during preheat.
 
 #define SMART_HERMS_HLT
-#define MASH_HEAT_LOSS 2.6
+#define MASH_HEAT_LOSS 2.2
 #define HLT_MAX_TEMP 180
 #define SMART_HERMS_PREHEAT
 //**********************************************************************************
